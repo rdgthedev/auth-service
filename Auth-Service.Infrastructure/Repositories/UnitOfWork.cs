9c8f2c15e2185d8d813f8b1dd7e31f97d7d94b01
Rodrigo Ferreira <rodrigo.ferreiradaslv@gmail.com>
2025-02-12 17:45:23 -0300

    refactor: adjusted UnitOfWork

--- Auth-Service.Infrastructure/Repositories/UnitOfWork.cs
+++ Auth-Service.Infrastructure/Repositories/UnitOfWork.cs
@@ -6,26 +6,16 @@ namespace Auth_Service.Infrastructure.Repositories;
 public class UnitOfWork : IUnitOfWork, IDisposable
 {
     private readonly AuthDbContext _context;
-    private IUserRepository _userRepository;
-    private IRoleRepository _roleRepository;
-    private IRefreshTokenRepository _refreshTokenRepository;
+    private IUserRepository _users;
+    private IRoleRepository _roles;
+    private IRefreshTokenRepository _refreshTokens;
 
-    public UnitOfWork(
-        AuthDbContext context,
-        IUserRepository userRepository,
-        IRoleRepository roleRepository,
-        IRefreshTokenRepository refreshTokenRepository
-    )
-    {
-        _context = context;
-        _userRepository = userRepository;
-        _roleRepository = roleRepository;
-        _refreshTokenRepository = refreshTokenRepository;
-    }
+    public UnitOfWork(AuthDbContext context)
+        => _context = context;
 
-    public IUserRepository Users => _userRepository ??= new UserRepository(_context);
-    public IRoleRepository Roles => _roleRepository ??= new RoleRepository(_context);
-    public IRefreshTokenRepository RefreshTokens => _refreshTokenRepository ??= new RefreshTokenRepository(_context);
+    public IUserRepository Users => _users ??= new UserRepository(_context);
+    public IRoleRepository Roles => _roles ??= new RoleRepository(_context);
+    public IRefreshTokenRepository RefreshTokens => _refreshTokens ??= new RefreshTokenRepository(_context);
 
     public async Task CommitAsync(CancellationToken cancellationToken)
         => await _context.SaveChangesAsync(cancellationToken);
